355a515d7787243efc7aee95e7298aa98f655ee1
Tauseef Ahmad <ertauseefahmad@gmail.com>
2026-02-12 18:32:29 +0530

    day5 done

---
+++ day5/src/app.js
@@ -0,0 +1,40 @@
+const express = require("express")
+
+const app = express() //* server create ho gya h
+
+app.use(express.json())
+
+const notes = []
+
+app.post("/notes", (req,res)=>{
+    notes.push(req.body)
+
+    res.status(201).json({
+        message:"notes created"
+    })
+    
+})
+
+app.get('/notes',(req,res)=>{
+    res.status(200).json({
+        notes: notes
+    })
+})
+
+app.delete('/notes/:index',(req,res)=>{
+    delete notes[req.params.index ]
+
+    res.status(204).json({
+        message:"notes deleted"
+    })
+})
+
+app.patch("/notes/:index",(req,res) => {
+    notes[req.params.index ].description = req.body.description 
+
+    res.status(200).json({
+        message:"notes updated"
+    })
+})
+
+module.exports = app
