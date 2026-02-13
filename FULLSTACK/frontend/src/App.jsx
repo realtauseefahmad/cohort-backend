a3d5a0f866aa8917b9ab39459f993d6da61d5bd4
Tauseef Ahmad <ertauseefahmad@gmail.com>
2026-02-13 22:23:08 +0530

    done

--- FULLSTACK/frontend/src/App.jsx
+++ FULLSTACK/frontend/src/App.jsx
@@ -1,40 +1,66 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
 
-  const [notes, setNotes] = useState([
-    {
-      title: "test title 1",
-      description: "test description"
-    },
-    {
-      title: "test title 2",
-      description: "test description"
-    },
-    {
-      title: "test title 3",
-      description: "test description"
-    },
-    {
-      title: "test title 4",
-      description: "test description"
-    }
-  ])
+  const [notes, setNotes] = useState([])
 
-  axios.get('http://localhost:3000/api/notes')
-  .then((res)=>{
-    setNotes(res.data.notes)
-  })
+
+  function fetchNotes() {
+    axios.get('http://localhost:3000/api/notes')
+      .then( res => {
+        setNotes(res.data.notes)
+      })
+  }
+
+
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const { title, description } = e.target.elements 
+
+    axios.post('http://localhost:3000/api/notes' , {
+      title: title.value,
+      description: description.value
+    })
+      .then(res => {
+ 
+        fetchNotes()
+
+      })
+
+  }
+
+  function handleDeleteNote(noteId){
+    axios.delete('http://localhost:3000/api/notes/'+noteId)
+    .then(res=>{
+
+      fetchNotes()
+      
+    })
+    
+  }
 
   return (
     <>
+
+      <form className='note-create-form' onSubmit={handleSubmit} >
+        <input type="text" placeholder='Enter Title' name='title' />
+        <input type="text" placeholder='Enter Description' name="description"/>
+        <button type="submit">Create Note</button>
+      </form>
+
       <div className="notes">
         {
           notes.map(note => {
             return <div className="note">
               <h1>{note.title}</h1>
               <p>{note.description}</p>
+              <button onClick={()=>{handleDeleteNote(note._id)}}>Delete</button>
             </div>
           })
         }
