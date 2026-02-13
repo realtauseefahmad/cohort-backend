2c2247bf6a16661e36535569f9b63c53eb9c6fba
Tauseef Ahmad <ertauseefahmad@gmail.com>
2026-02-13 17:51:01 +0530

    complete backend

---
+++ FULLSTACK/backend/src/app.js
@@ -0,0 +1,49 @@
+const express = require("express")
+const noteModel = require("./models/notes.model")
+
+const app = express()
+app.use(express.json())
+
+app.post("/api/notes", async (req,res)=>{
+    const {title , description} = req.body
+
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        message:"Note created successfully",
+        note
+        
+    })
+})
+
+app.get("/api/notes", async (req,res)=>{
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message:"Data Fetched successfully",
+        notes
+    })
+})
+
+app.delete("/api/notes/:id", async (req,res)=>{
+    const id = req.params.id 
+
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message:"Note deleted successfully"
+    })
+})
+
+app.patch("/api/notes/:id", async (req,res)=>{
+    const id = req.params.id
+    const {description} = req.body 
+    await noteModel.findByIdAndUpdate(id , {description})
+    res.status(200).json({
+        message:"Note updated successfully"
+    })
+})
+
+module.exports = app
