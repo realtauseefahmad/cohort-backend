70203233a4ee53ddfbc91dcc5e88bc5b3e675f0e
Tauseef Ahmad <ertauseefahmad@gmail.com>
2026-02-15 02:03:45 +0530

    authentication and login system and bcrypt.js

--- day11/src/routes/auth.routes.js
+++ day11/src/routes/auth.routes.js
@@ -44,6 +44,20 @@ authRoutes.post("/protected", (req, res) => {
     })
 })
 
+authRoutes.get("/get-me", async (req,res)=>{
+    const token = req.cookies.token
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    const user = await userModel.findById(decoded.id)
+
+    res.json({
+        name: user.name,
+        email: user.email,
+    })
+})
+
+
 
 authRoutes.post("/login", async (req, res) => {
     const { email, password } = req.body
