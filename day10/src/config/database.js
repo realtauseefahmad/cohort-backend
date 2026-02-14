36ebd61b15230bcf99ec2a793f943e0ea9dea54e
Tauseef Ahmad <ertauseefahmad@gmail.com>
2026-02-14 23:46:50 +0530

    authentication

---
+++ day10/src/config/database.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+
+
+function connectToDB() {
+    mongoose.connect(process.env.MONGO_URL)
+        .then(() => {
+            console.log("Database is connected")
+        })
+}
+
+module.exports = connectToDB
